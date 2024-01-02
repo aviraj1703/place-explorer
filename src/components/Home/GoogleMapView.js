31c41e23dc1624f2530cd3258de493434db13a42
aviraj1703 <avirajrathod2002.ar@gmail.com>
2024-01-02 23:33:18 +0530

    region added

--- src/components/Home/GoogleMapView.js
+++ src/components/Home/GoogleMapView.js
@@ -1,14 +1,23 @@
 import { StyleSheet, View, Text } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-import React from "react";
+import React, { useState } from "react";
 
 export default function GoogleMapView() {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 12.9237127,
+    longitude: 77.6721937,
+    latitudeDelta: 0.01,
+    longitudeDelta: 0.01,
+  });
+
   return (
     <View style={styles.container}>
       <MapView
         style={styles.map}
         provider={PROVIDER_GOOGLE}
+        region={mapRegion}
         showsUserLocation={true}
+        showsMyLocationButton={true}
       ></MapView>
     </View>
   );
